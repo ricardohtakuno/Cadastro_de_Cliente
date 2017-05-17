2905ff4f67254fefedcdf1180c06f0c2d74e0d1c
ricardo.htakuno <ricardo.htakuno@AME0556344W10-1>
2017-05-16 22:23:11 -0300

    Criação de duas classes (clPedidos e clItensPedido)

---
+++ Dados do Cliente/AcessoDB/clItensPedido.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class clItensPedido
+    {
+        //propriedades
+        public string banco { get; set; }
+        public int ID_Item { get; set; }
+        public int ID_Pedido { get; set; }
+        public int ID_Produto { get; set; }
+        public string Qtde { get; set; }
+        public string Unitario { get; set; }
+        public string Subtotal { get; set; }
+        public void Gravar()
+        {
+            //variável utilizada para "concatenar" texto de forma estruturada
+            StringBuilder strQuery = new StringBuilder();
+
+            //montagem do INSERT
+            strQuery.Append("INSERT INTO tbItensPedido");
+
+            strQuery.Append(" ( ");
+
+            strQuery.Append(" ID_Pedido ");
+            strQuery.Append(", ID_Produto ");
+            strQuery.Append(", Qtde ");
+            strQuery.Append(", Unitario ");
+            strQuery.Append(", Subtotal ");
+
+            strQuery.Append(" ) ");
+
+            strQuery.Append(" VALUES ( ");
+
+            strQuery.Append("'" + ID_Pedido + "'");
+            strQuery.Append(",'" + ID_Produto + "'");
+            strQuery.Append(",'" + Qtde.Replace(",", ".") + "'");
+            strQuery.Append(",'" + Unitario.Replace(",", ".") + "'");
+            strQuery.Append(",'" + Subtotal.Replace(",", ".") + "'");
+            
+            strQuery.Append(" ); ");
+
+            //instancia a classe clAcessoDB e executa o comando
+            clAcessoDB clAcessoDB = new clAcessoDB();
+            clAcessoDB.vConexao = banco;
+            clAcessoDB.ExecutaComando(strQuery.ToString());
+        }
+        public void Alterar()
+        {
+            StringBuilder strQuery = new StringBuilder();
+
+            //montagem de update
+            strQuery.Append("UPDATE tbItensPedido");
+
+            strQuery.Append(" SET ");
+
+            strQuery.Append(" ID_Pedido = '" + ID_Pedido + "'");
+            strQuery.Append(", ID_Produto = '" + ID_Produto + "'");
+            strQuery.Append(", Qtde = '" + Qtde.Replace(",", ".") + "'");
+            strQuery.Append(", Unitario = '" + Unitario.Replace(",", ".") + "'");
+            strQuery.Append(", Subtotal = '" + Subtotal.Replace(",", ".") + "'");
+
+            strQuery.Append(" WHERE ");
+
+            strQuery.Append(" ID_Item = " + ID_Item);
+
+            //instancia a classe clAcessoDB e executa o comando
+            clAcessoDB clAcessoDB = new clAcessoDB();
+            clAcessoDB.vConexao = banco;
+            clAcessoDB.ExecutaComando(strQuery.ToString());
+        }
+        public void Excluir()
+        {
+            StringBuilder strQuery = new StringBuilder();
+
+            //montagem do delete
+            strQuery.Append(" DELETE FROM tbItensPedido ");
+            strQuery.Append(" WHERE ");
+            strQuery.Append(" ID_Item = " + ID_Item);
+
+            //instancia a classe clAcessoDB e executa o comando
+            clAcessoDB clAcessoDB = new clAcessoDB();
+            clAcessoDB.vConexao = banco;
+            clAcessoDB.ExecutaComando(strQuery.ToString());
+        }
+    }
+}
---
+++ Dados do Cliente/AcessoDB/clPedidos.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class clPedidos
+    {
+        //propriedades
+        public string banco { get; set; }
+        public int ID_Pedido { get; set; }
+        public string Data { get; set; }
+        public int Cliente { get; set; }
+        public void Gravar()
+        {
+            //variável utilizada para "concatenar" texto de forma estruturada
+            StringBuilder strQuery = new StringBuilder();
+
+            //montagem do INSERT
+            strQuery.Append("INSERT INTO tbPedidos");
+
+            strQuery.Append(" ( ");
+
+            strQuery.Append(" Data ");
+            strQuery.Append(", Cliente ");
+
+            strQuery.Append(" ) ");
+
+            strQuery.Append(" VALUES ( ");
+
+            strQuery.Append("'" + Data + "'");
+            strQuery.Append(",'" + Cliente + "'");
+
+            strQuery.Append(" ); ");
+
+            //instancia a classe clAcessoDB e executa o comando
+            clAcessoDB clAcessoDB = new clAcessoDB();
+            clAcessoDB.vConexao = banco;
+            clAcessoDB.ExecutaComando(strQuery.ToString());
+        }
+        public void Alterar()
+        {
+            StringBuilder strQuery = new StringBuilder();
+
+            //montagem de update
+            strQuery.Append("UPDATE tbPedidos");
+
+            strQuery.Append(" SET ");
+
+            strQuery.Append(" Data = '" + Data + "'");
+            strQuery.Append(", Cliente = '" + Cliente + "'");
+
+            strQuery.Append(" WHERE ");
+
+            strQuery.Append(" ID_Pedido = " + ID_Pedido);
+
+            //instancia a classe clAcessoDB e executa o comando
+            clAcessoDB clAcessoDB = new clAcessoDB();
+            clAcessoDB.vConexao = banco;
+            clAcessoDB.ExecutaComando(strQuery.ToString());
+        }
+        public void Excluir()
+        {
+            StringBuilder strQuery = new StringBuilder();
+
+            //montagem do delete
+            strQuery.Append(" DELETE FROM tbPedidos ");
+            strQuery.Append(" WHERE ");
+            strQuery.Append(" ID_Pedido = " + ID_Pedido);
+
+            //instancia a classe clAcessoDB e executa o comando
+            clAcessoDB clAcessoDB = new clAcessoDB();
+            clAcessoDB.vConexao = banco;
+            clAcessoDB.ExecutaComando(strQuery.ToString());
+        }
+    }
+}
